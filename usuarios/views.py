c7e439c1512e2a11fd225f77c542d8f05d14f8ed
Raphael-Soares <soaresphaell@gmail.com>
2023-07-17 15:56:57 -0300

    django forms

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,16 +1,24 @@
 from django.shortcuts import render
+from usuarios.forms import LoginForms, CadastroForms
 
 # Create your views here.
 
 
 def login(request):
+    form = LoginForms
+
+    
 
     return render (
-        request, 'usuarios/login.html'
+        request, 'usuarios/login.html', {'form': form}
     )
 
 def cadastro(request):
 
+
+    form = CadastroForms
+
+
     return render (
-        request,"usuarios/cadastro.html"
+        request,"usuarios/cadastro.html", {'form': form}
     )
