3d0ce4e17b4822e2bdafb043c6935822d3e3cdd3
Raphael-Soares <soaresphaell@gmail.com>
2023-07-17 15:04:47 -0300

    usuarios url

--- core/settings.py
+++ core/settings.py
@@ -32,7 +32,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "galeria.apps.GaleriaConfig",
-    "usuarios.app.GaleriaConfig"
+    "usuarios.apps.UsuariosConfig"
 ]
 
 MIDDLEWARE = [
--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
+from usuarios.views import login,cadastro
 
 
 
 urlpatterns = [
+    path('login', login, name='login'),
+    path('cadastro', cadastro, name='cadastro'),
 
   
 ]
