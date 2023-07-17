6ca54a4ddc445a360eb08b4f6f7beddfe23f94b6
Raphael Soares <raphael.soares@unimedsc.coop.br>
2023-07-17 14:47:40 -0300

    overflow fixed

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path, include
-from galeria.views import index, imagem
+from galeria.views import index, imagem,buscar
+
+
 
 urlpatterns = [
     path('', index, name='home'),
-    path('imagem/<int:foto_id>', imagem, name='imagem')
+    path('imagem/<int:foto_id>', imagem, name='imagem'),
+    path('buscar/', buscar, name='buscar')
+  
 ]
--- galeria/views.py
+++ galeria/views.py
@@ -20,6 +20,19 @@ def imagem(request, foto_id):
         request, 'galeria/imagem.html', {"fotografia": fotografia}
     )
 
+def buscar(request):
+
+    fotografias = Fotografia.objects.order_by("data_fotografia").filter(publicado=True)
+
+    if 'buscar' in request.GET:
+        nome_a_buscar = request.GET['buscar']
+        if nome_a_buscar:
+            fotografias = fotografias.filter(nome__icontains=nome_a_buscar)
+
+    
+    return render(
+        request, 'galeria/buscar.html', {"cards": fotografias}
+    )
 
 
 
