d7eccb9e015b047f642a66537c61e6d3078636d3
Raphael-Soares <soaresphaell@gmail.com>
2023-07-18 14:48:24 -0300

    Mensagens de erro

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -1,6 +1,5 @@
 from django import forms
 
-
 class LoginForms(forms.Form):
     nome_login=forms.CharField(
         label='Nome de Login', 
@@ -20,16 +19,14 @@ class LoginForms(forms.Form):
         widget=forms.PasswordInput(
             attrs={
                 'class': 'form-control',
-                'placeholder': 'Digite sua senha',
+                'placeholder': 'Digite a sua senha',
             }
         ),
     )
 
-
-
 class CadastroForms(forms.Form):
     nome_cadastro=forms.CharField(
-        label='Nome de Login', 
+        label='Nome de Cadastro', 
         required=True, 
         max_length=100,
         widget=forms.TextInput(
@@ -39,12 +36,17 @@ class CadastroForms(forms.Form):
             }
         )
     )
-
-    email_cadastro = forms.EmailField(label='Email para cadastro', required=True, max_length=100,widget=forms.EmailInput(attrs={
-        'class': 'form-control',
-        'placeholder':'Ex.: joaosilva@xpto.com'
-    }))
-
+    email=forms.EmailField(
+        label='Email',
+        required=True,
+        max_length=100,
+        widget=forms.EmailInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Ex.: joaosilva@xpto.com',
+            }
+        )
+    )
     senha_1=forms.CharField(
         label='Senha', 
         required=True, 
@@ -52,31 +54,38 @@ class CadastroForms(forms.Form):
         widget=forms.PasswordInput(
             attrs={
                 'class': 'form-control',
-                'placeholder': 'Digite sua senha',
+                'placeholder': 'Digite a sua senha',
             }
         ),
     )
     senha_2=forms.CharField(
-        label='Confirme sua senha', 
+        label='Confirme a sua senha', 
         required=True, 
         max_length=70,
         widget=forms.PasswordInput(
             attrs={
                 'class': 'form-control',
-                'placeholder': 'Digite sua senha novamente',
+                'placeholder': 'Digite a sua senha novamente',
             }
         ),
-    ) 
-
+    )
 
     def clean_nome_cadastro(self):
-        nome = self.cleaned_data.get("nome_cadastro")
+        nome = self.cleaned_data.get('nome_cadastro')
 
         if nome:
             nome = nome.strip()
-            if " " in nome:
-                raise forms.ValidationError("Não é possível inserir espaços dentro do campo usuário.")
+            if ' ' in nome:
+                raise forms.ValidationError('Espaços não são permitidos nesse campo')
             else:
                 return nome
-            
 
+    def clean_senha_2(self):
+        senha_1 = self.cleaned_data.get('senha_1')
+        senha_2 = self.cleaned_data.get('senha_2')
+
+        if senha_1 and senha_2:
+            if senha_1 != senha_2:
+                raise forms.ValidationError('Senhas não são iguais')
+            else:
+                return senha_2
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,18 +1,16 @@
 from django.shortcuts import render, redirect
+
 from usuarios.forms import LoginForms, CadastroForms
+
 from django.contrib.auth.models import User
+
 from django.contrib import auth
+
 from django.contrib import messages
 
-
-
-# Create your views here.
-
-
 def login(request):
-    form = LoginForms
+    form = LoginForms()
 
-    
     if request.method == 'POST':
         form = LoginForms(request.POST)
 
@@ -20,54 +18,48 @@ def login(request):
             nome = form['nome_login'].value()
             senha = form['senha'].value()
 
+        usuario = auth.authenticate(
+            request,
+            username=nome,
+            password=senha
+        )
+        if usuario is not None:
+            auth.login(request, usuario)
+            messages.success(request, f'{nome} logado com sucesso!')
+            return redirect('index')
+        else:
+            messages.error(request, 'Erro ao efetuar login')
+            return redirect('login')
 
-            user = auth.authenticate(request,username=nome,password=senha)
-
-            if user is not None:
-                auth.login(request, user)
-                messages.success(request, f"{nome} logado com sucesso")
-                return redirect('index')
-            else:
-                messages.error(request, "Erro ao efetuar login")
-                return redirect('login')
-
-        
-    return render (
-        request, 'usuarios/login.html', {'form': form}
-    )
+    return render(request, 'usuarios/login.html', {'form': form})
 
 def cadastro(request):
-    form = CadastroForms
-
+    form = CadastroForms()
 
     if request.method == 'POST':
-        form = CadastroForms(request.POST) 
-
-
-        if form['senha_1'].value() != form['senha_2'].value():
-            return redirect('cadastro')
-        
+        form = CadastroForms(request.POST)
 
         if form.is_valid():
-            nome = form['nome_cadastro'].value()
-            email = form['email_cadastro'].value()
-            senha = form['senha_1'].value()
+            nome=form['nome_cadastro'].value()
+            email=form['email'].value()
+            senha=form['senha_1'].value()
 
             if User.objects.filter(username=nome).exists():
                 messages.error(request, 'Usuário já existente')
                 return redirect('cadastro')
 
-            user = User.objects.create_user(username=nome, email=email,password=senha)
-            user.save()
+            usuario = User.objects.create_user(
+                username=nome,
+                email=email,
+                password=senha
+            )
+            usuario.save()
             messages.success(request, 'Cadastro efetuado com sucesso!')
             return redirect('login')
 
-    return render (
-        request,"usuarios/cadastro.html", {'form': form}
-    )
+    return render(request, 'usuarios/cadastro.html', {'form': form})
 
 def logout(request):
     auth.logout(request)
-    messages.success(request, "Logout efetuado com sucesso!")
-
-    return redirect("login")
+    messages.success(request, 'Logout efetuado com sucesso!')
+    return redirect('login')
