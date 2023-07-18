6e518d384f4093ead9ab76bff7264cddb2ba34cf
Raphael-Soares <soaresphaell@gmail.com>
2023-07-18 14:13:28 -0300

    associando tabelas

--- galeria/models.py
+++ galeria/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 from datetime import datetime
 
 
@@ -19,6 +20,13 @@ class Fotografia(models.Model):
     foto = models.ImageField(upload_to="fotos/%Y/%m/%d/", blank=True)
     publicado = models.BooleanField(default=False)
     data_fotografia = models.DateTimeField(default=datetime.now, blank=False)
+    usuario = models.ForeignKey(
+        to=User,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=False,
+        related_name='user'
+    )
 
 
     def __str__ (self):
